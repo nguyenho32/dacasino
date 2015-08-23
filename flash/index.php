d991e6a6a522930f3abe457a69df6fb99a72d5af
master <debug@juxtaflows.com>
2015-08-22 17:11:02 -0700

    this is a test commit?

---
+++ flash/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+
+<style>
+	#container {
+		width:800px;
+		height:700px;
+		border: 15px solid grey;
+		margin: auto;
+	}
+</style>
+</head>
+
+<body>
+<a href="classic-playing-cards.png">imap</a>
+<div id="container">
+
+<object type="application/x-shockwave-flash" data="Main.swf" 
+width="800" height="700" class="ad">
+  <param name="movie" value="Main.swf" />
+  <param value="high" name="quality">
+  <param value="#FFFFFF" name="bgcolor">
+  <param value="always" name="allowScriptAccess">
+  <param value="clickTAG=http://www.google.com/" name="FlashVars">
+</object>
+</div>
+</body>
+</html>
